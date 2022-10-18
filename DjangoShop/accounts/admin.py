ee7cad2c112cbb1dfb57c9fc2ed33a985675b8cb
afshintrg <ataraghijoo@gmial.com>
2022-10-18 14:39:58 +0330

    Add Custom User Model

--- DjangoShop/accounts/admin.py
+++ DjangoShop/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'phone_number')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', )}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email', 'phone_number', )}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(Group)
+
