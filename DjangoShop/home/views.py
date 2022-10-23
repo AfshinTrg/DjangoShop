e67ff23931497352d9b38554afc698807acee350
afshintrg <ataraghijoo@gmial.com>
2022-10-23 19:56:51 +0330

    Add HomeView detail -show categories -show products

--- DjangoShop/home/views.py
+++ DjangoShop/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category, Product
 
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request, 'home/home.html')
+        products = Product.objects.filter(available=True)
+        categories = Category.objects.filter(is_sub=False)
+        return render(request, 'home/home.html', {'products': products, 'categories': categories})
+
 
