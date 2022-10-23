b2e59a2f2a0a257625780e00a484c02f7fd1e4b5
afshintrg <ataraghijoo@gmial.com>
2022-10-23 18:55:17 +0330

    Add Category Model

--- DjangoShop/home/models.py
+++ DjangoShop/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='scategory', null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        if self.sub_category:
+            return f'{self.sub_category}------->***{self.name}***'
+        return f'{self.name}'
+
