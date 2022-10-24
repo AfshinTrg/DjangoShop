5a25e6a3ec03d1fa02bf8d7996719ddb7189bff9
afshintrg <ataraghijoo@gmial.com>
2022-10-24 11:29:46 +0330

    Add product detail page

--- DjangoShop/home/models.py
+++ DjangoShop/home/models.py
@@ -31,4 +31,7 @@ class Product(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.slug])
+
 
--- DjangoShop/home/views.py
+++ DjangoShop/home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Category, Product
 
 
 class HomeView(View):
+    template_name = 'home/home.html'
 
     def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
@@ -11,6 +12,12 @@ class HomeView(View):
         if category_slug:
             category = Category.objects.get(slug=category_slug)
             products = products.filter(category=category)
-        return render(request, 'home/home.html', {'products': products, 'categories': categories})
+        return render(request, self.template_name, {'products': products, 'categories': categories})
 
 
+class ProductDetailView(View):
+    template_name = 'home/detail.html'
+
+    def get(self, request, product_slug):
+        product = get_object_or_404(Product, slug=product_slug)
+        return render(request, self.template_name, {'product': product})
