d2d1aa0159cfad26c3b5fedfc9378525f0ced84b
afshintrg <ataraghijoo@gmial.com>
2022-10-24 16:19:30 +0330

    Add total price feature for cart

--- DjangoShop/orders/cart.py
+++ DjangoShop/orders/cart.py
@@ -24,6 +24,9 @@ class Cart:
             del self.cart[p_id]
         self.save()
 
+    def get_total_price(self):
+        return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
+
     def __iter__(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
